22eef1bcc0142a57adb93a437ac6d2c8ab16aa6f
ZakeryKeahey <zak2151373@maricopa.edu>
2021-09-16 19:30:46 -0700

    Card Activity

---
+++ card-activity/src/main/java/com/example/carddeck/DeckDriver.java
@@ -0,0 +1,18 @@
+package com.example.carddeck;
+
+public class DeckDriver {
+
+    public static void main(String[] args) {
+
+        Deck deck = new StandardDeck();
+
+        System.out.println(deck);
+
+
+        deck.shuffle();
+
+        System.out.println(deck);
+    }
+
+
+}
